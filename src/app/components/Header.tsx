a165b993a4d90ad47029d647bddc086506be7c10
gideonadeti <gideonadeti0@gmail.com>
2024-10-12 14:28:51 +0000

    Work on Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="text-center shadow-sm p-2 container-fluid">
+      <h1>URL Shortener</h1>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="d-flex flex-column min-vh-100">
-      
+      <Header />
     </div>
   );
 }
