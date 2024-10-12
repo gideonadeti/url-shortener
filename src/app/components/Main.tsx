bfa94c69f57ca8ab7a99ef275fc7691ddf0026a1
gideonadeti <gideonadeti0@gmail.com>
2024-10-12 15:52:24 +0000

    Add bootstrap icons and make some adjustments

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -12,7 +12,7 @@ export default function Main() {
       >
         <select
           className="form-select mb-3"
-          onChange={(e) => setMethod(e.target.value)}
+          onChange={({ target }) => setMethod(target.value)}
         >
           <option value="create">Create</option>
           <option value="read">Read</option>
@@ -30,6 +30,103 @@ export default function Main() {
                 placeholder=""
               />
               <label htmlFor="longUrl">Paste or enter long URL</label>
+              <button
+                type="submit"
+                className="btn position-absolute top-0 bottom-0 end-0 border border-start-0 rounded-end bg-white"
+                style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+              >
+                <i className="bi-play fs-3"></i>
+              </button>
+            </div>
+          </form>
+        )}
+        {method === "read" && (
+          <form className="w-100">
+            <div className="form-floating mb-3">
+              <input
+                type="url"
+                className="form-control"
+                name="shortUrl"
+                id="shortUrl"
+                placeholder=""
+              />
+              <label htmlFor="shortUrl">Paste or enter short URL</label>
+              <button
+                type="submit"
+                className="btn position-absolute top-0 bottom-0 end-0 border border-start-0 rounded-end bg-white"
+                style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+              >
+                <i className="bi-play fs-3"></i>
+              </button>
+            </div>
+          </form>
+        )}
+        {method === "update" && (
+          <form className="w-100">
+            <div className="form-floating mb-3">
+              <input
+                type="url"
+                className="form-control"
+                name="shortUrl"
+                id="shortUrl"
+                placeholder=""
+              />
+              <label htmlFor="shortUrl">Paste or enter short URL</label>
+              <button
+                type="submit"
+                className="btn position-absolute top-0 bottom-0 end-0 border border-start-0 rounded-end bg-white"
+                style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+              >
+                <i className="bi-play fs-3"></i>
+              </button>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="url"
+                className="form-control"
+                name="longUrl"
+                id="longUrl"
+                placeholder=""
+              />
+              <label htmlFor="longUrl">
+                Paste or enter new long URL{" "}
+                <small className="form-text">
+                  (Note: Old long URL will be replaced)
+                </small>
+              </label>
+              <button
+                type="submit"
+                className="btn position-absolute top-0 bottom-0 end-0 border border-start-0 rounded-end bg-white"
+                style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+              >
+                <i className="bi-play fs-3"></i>
+              </button>
+            </div>
+          </form>
+        )}
+        {method === "delete" && (
+          <form className="w-100">
+            <div className="form-floating mb-3">
+              <input
+                type="url"
+                className="form-control"
+                name="shortUrl"
+                id="shortUrl"
+                placeholder=""
+              />
+              <label htmlFor="shortUrl">
+                Paste or enter short URL{" "}
+                <small className="text-danger form-text">
+                  (Note: This cannot be undone)
+                </small>
+              </label>
+              <button
+                type="submit"
+                className="btn position-absolute top-0 bottom-0 end-0 border border-start-0 rounded-end bg-white"
+                style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+              >
+                <i className="bi-play fs-3"></i>
+              </button>
             </div>
           </form>
         )}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 
