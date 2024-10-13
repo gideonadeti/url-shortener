2a32132ec6f0cebc359e7d9b21d102181b669228
gideonadeti <gideonadeti0@gmail.com>
2024-10-13 21:11:03 +0000

    Set only padding y

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="text-center shadow-sm p-2 container-fluid">
+    <header className="text-center shadow-sm py-2 container-fluid">
       <h1>URL Shortener</h1>
     </header>
   );
